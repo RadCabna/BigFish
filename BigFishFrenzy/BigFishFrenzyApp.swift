babb7a10063568cc16eb79f2b43003b69fade16d
Radcabna <radcabna@gmail.com>
2025-01-20 00:34:50 +0300

    Initial Commit

---
+++ BigFishFrenzy/BigFishFrenzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigFishFrenzyApp.swift
+//  BigFishFrenzy
+//
+//  Created by Алкександр Степанов on 20.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BigFishFrenzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
