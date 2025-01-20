ea8dd1aab41dce8045370ff7091efd2ee21409b8
Radcabna <radcabna@gmail.com>
2025-01-20 18:19:41 +0300

    First

---
+++ BigFishFrenzy/Views/Loading.swift
@@ -0,0 +1,18 @@
+//
+//  Loading.swift
+//  BigFishFrenzy
+//
+//  Created by Алкександр Степанов on 20.01.2025.
+//
+
+import SwiftUI
+
+struct Loading: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Loading()
+}
